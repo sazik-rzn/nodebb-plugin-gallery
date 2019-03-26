66400ea4b90829515be26001e194863400c1b835
vladimir <cfpbr/cfpbr>
2019-03-26 17:49:16 +0300

    first

--- public/lib/main.js
+++ public/lib/main.js
@@ -3,8 +3,10 @@
 
 	jQuery('document').ready(function() {
 		$(window).on('action:ajaxify.end', function() {
-			var img = $('[component="post/content"] .img-markdown');
-			img.parent('a').addClass('fancybox').attr('rel', 'gallery').fancybox();
+			// var img = $('[component="post/content"] img');
+			// var topic
+			// img.parent('a').addClass('fancybox').attr('rel', 'gallery').fancybox();
+			lightGallery($('[component="post/content"]'));
 		});
 	});
 }());
